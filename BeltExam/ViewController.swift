b8f1c309ebc69f46aceb1b5f3ab1e80d238c8370
El Capitan <Elcapitan@els-macbook-3.local>
2018-07-20 09:17:44 -0700

    Initial Commit

---
+++ BeltExam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeltExam
+//
+//  Created by El Capitan on 7/20/18.
+//  Copyright © 2018 Carlo Del Mundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
